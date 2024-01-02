b59b444a57c34fb111243c455a92705830f35dba
bsr7 <bsr7cxp@virginia.edu>
2024-01-02 15:09:24 -0500

    my initial changes to resume React app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import {EE} from './EE.jsx';
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Byron Richards</h1>
+     <EE title="Education" item1="UVA" item2="CS" item3="DSA"/>
+     <EE title="Experience" item1="SWE" item2="worked on app" item3="won award"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/EE.jsx
@@ -0,0 +1,14 @@
+
+export const EE = ({title, item1, item2, item3}) => {
+
+    return (
+        <div>
+            <h3>{title}</h3>
+            <ul>
+                <li>{item1}</li>
+                <li>{item2}</li>
+                <li>{item3}</li>
+            </ul>
+        </div>
+    )
+}
